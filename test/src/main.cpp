2393ec653b0750b3d2230c309586325633d09f5c
Julian Churchill <chewielouie03@gmail.com>
2012-08-31 11:58:47 +0100

    Added an extra branch to the test program

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,7 +5,12 @@ int main( int argc, char** argv )
     bool run = false;
     if( run )
         printf( "0th path" );
-    else
+    else {
+        if( run )
+            run = true;
+        else
+            run = false;
         printf( "1st path" );
+    }
     return 0;
 }
