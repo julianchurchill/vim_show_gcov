ae54b861297951cf6d0f3d05fa4e44f9e0bf619c
Julian Churchill <chewielouie03@gmail.com>
2012-09-02 20:06:02 +0100

    Tidied up based on flake8 output \(python lint\)

--- show_gcov.py
+++ show_gcov.py
@@ -1,6 +1,7 @@
 import os
 
-def find_gcov_file( full_cpp_path ):
-    if not os.path.exists( full_cpp_path + ".gcov" ):
+
+def find_gcov_file(full_cpp_path):
+    if not os.path.exists(full_cpp_path + ".gcov"):
         return ""
     return full_cpp_path + ".gcov"
--- show_gcov_tests.py
+++ show_gcov_tests.py
@@ -3,26 +3,28 @@ import show_gcov
 import os
 import shutil
 
+
 class ShowGcovTests(unittest.TestCase):
 
     def setUp(self):
         self.start_dir = "/tmp/test_dir" + str(os.getpid())
-        if not os.path.exists( self.start_dir ):
-            os.makedirs( self.start_dir )
+        if not os.path.exists(self.start_dir):
+            os.makedirs(self.start_dir)
 
     def tearDown(self):
-        shutil.rmtree( self.start_dir )
+        shutil.rmtree(self.start_dir)
 
     def test_should_return_gcov_file_in_start_directory_if_there(self):
         full_cpp_path = self.start_dir + "/main.cpp"
-        f = open( full_cpp_path + '.gcov', 'w' )
-        f.write( "some gcov file content" )
+        f = open(full_cpp_path + '.gcov', 'w')
+        f.write("some gcov file content")
         f.close
-        self.assertEquals( show_gcov.find_gcov_file( full_cpp_path ), full_cpp_path + ".gcov" )
+        self.assertEquals(show_gcov.find_gcov_file(full_cpp_path),
+                          full_cpp_path + ".gcov")
 
     def test_should_return_blank_if_gcov_file_not_found(self):
         full_cpp_path = self.start_dir + "/main.cpp"
-        self.assertEquals( show_gcov.find_gcov_file( full_cpp_path ), "" )
+        self.assertEquals(show_gcov.find_gcov_file(full_cpp_path), "")
 
     #def test_should_only_match_gcov_file_with_same_name_as_input_file(self):
 
