06db49fcfa49a07fee46bc443d70d9c1c0fbe091
Julian Churchill <chewielouie03@gmail.com>
2012-09-02 22:08:00 +0100

    REFACTOR: Added result member to simplify return values for FindGcovFile class

--- show_gcov.py
+++ show_gcov.py
@@ -12,24 +12,28 @@ class FindGcovFile:
     def __init__(self, full_cpp_path):
         self.full_cpp_path = full_cpp_path
         self.filename = os.path.basename(self.full_cpp_path)
+        self.initial_search_dir = os.path.dirname(self.full_cpp_path)
+        self.result = ""
 
     def find_gcov_file(self):
-        if os.path.exists(self.full_cpp_path + FindGcovFile.gcov_extension):
-            return self.full_cpp_path + FindGcovFile.gcov_extension
-        return self.search_subdirs()
+        if os.path.exists(self.make_gcov_filename(self.initial_search_dir)):
+            self.result = self.make_gcov_filename(self.initial_search_dir)
+        else:
+            self.search_subdirs()
+        return self.result
 
     def search_subdirs(self):
-        initial_search_dir = os.path.dirname(self.full_cpp_path)
-        for entry in os.listdir(initial_search_dir):
-            file_path = self.find_gcov_file_in_directory(
-                    os.path.join(initial_search_dir, entry))
-            if file_path != "":
-                return file_path
-        return ""
+        for entry in os.listdir(self.initial_search_dir):
+            self.find_gcov_file_in_directory(
+                    os.path.join(self.initial_search_dir, entry))
+            if self.result != "":
+                return
 
     def find_gcov_file_in_directory(self, path):
         if os.path.isdir(path):
-            gcov_path = os.path.join(path, self.filename) + FindGcovFile.gcov_extension
+            gcov_path = self.make_gcov_filename(path)
             if os.path.exists(gcov_path):
-                return gcov_path
-        return ""
+                self.result = gcov_path
+
+    def make_gcov_filename(self, path):
+        return os.path.join(path, self.filename) + FindGcovFile.gcov_extension
